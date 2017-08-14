a2fdede4333d66513f6036948b4cacbcf412a33a
Tom Counsell <contact@tomcounsell.com>
2017-08-14 19:56:37 +0200

    handle exceptions in indicators on 3 tiers

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import os
 from logging import basicConfig, INFO
 from google.cloud import datastore
 from google.cloud import pubsub
-from indicators import price, sma, ema
+from indicators import price, price_average, sma, ema
 
 
 basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
@@ -16,9 +16,15 @@ tier_1_indicators = [
     price.price,
 ]
 
-# Tier 2 indicators have one or more dependaencies on Tier 1 indicaotrs
+# Tier 2 indicators have one or more dependencies on Tier 1 indicaotrs
 # They should run after all Tier 1 indicators have completed processing
 tier_2_indicators = [
+    price_average.price_average
+]
+
+# Tier 3 indicators have one or more dependencies on Tier 2 indicaotrs
+# They should run after all Tier 1 indicators have completed processing
+tier_3_indicators = [
     # sma.sma,
     # ema.ema,
 ]
@@ -44,9 +50,11 @@ def update_indicators(indicator_list, channel="Poloniex", refresh=False):
     # query.add_filter('channel', '=', channel)
     query.order = ['-timestamp']
     datastore_entities = list(query.fetch(limit=1))
-    
+
     for datastore_entity in datastore_entities:
         timestamp = datastore_entity['timestamp']
+
+    no_exceptions = True
 
     if refresh:
         # refresh all indicator data
@@ -56,8 +64,13 @@ def update_indicators(indicator_list, channel="Poloniex", refresh=False):
     else:
         # send to all indicator processors
         for indicator_function in indicator_list:
-            indicator_function(client, datastore_entity, timestamp)
+            try:
+                indicator_function(client, datastore_entity, timestamp)
+            except Exception as e:
+                print(str(e))
+                no_exceptions = False
 
+    return no_exceptions
 
 def main():
     """ Main """
@@ -71,35 +84,40 @@ def main():
         assert topic.exists()
         subscription = client.subscription('public-indicators-subscription')
         assert subscription.exists()
+        print("subscription activated")
 
         # while True:
         while True:
+            print("pulling messages")
             pulled = subscription.pull(max_messages=2)
             for ack_id, message in pulled:
 
                 try:
                     message = message.data.decode()
-                    print(message)
+                    print("received message: " + message)
 
                     refresh = ("new" not in message and "refresh" in message)
 
                     if "Poloniex" in message:
-                        if "data" in message:
-                            update_indicators(tier_1_indicators,
-                                              channel="Poloniex",
-                                              refresh=refresh)
-                            update_indicators(tier_2_indicators,
-                                              channel="Poloniex",
-                                              refresh=refresh)
+                        channel = "Poloniex"
+                    elif "Bittrex" in message:
+                        channel = "Bittrex"
+                    else:
+                        channel = None
 
-                    # elif "Bittrex" in message:
-                    #     if "data" in message:
-                    #         update_indicators(tier_1_indicators,
-                    #                           channel="Bittrex",
-                    #                           refresh=refresh)
-                    #         update_indicators(tier_2_indicators,
-                    #                           channel="Bittrex",
-                    #                           refresh=refresh)
+                    success = True
+
+                    if channel:
+                        if "data" in message:
+                            for indicator_tier_list in [
+                                tier_1_indicators,
+                                tier_2_indicators,
+                                tier_3_indicators,
+                            ]:
+                                if success: #continue if previous tier was successful
+                                    success = update_indicators(indicator_tier_list,
+                                                                channel=channel,
+                                                                refresh=refresh)
 
                 except AttributeError as e:
                     print(str(e))
